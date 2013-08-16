e39ce900c903d0fc94b629cbb344452c83beabd6
Apprentice <apprentice@dbc15.local>
2013-08-15 22:26:12 -0700

    finish

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,10 +3,11 @@ get '/' do
   erb :index
 end
 
-post '/color' do
+get '/colors' do
+  content_type :json
 
   #Create and return a JSON object with the random cell and color given below.
-
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  {cell: cell, color: color}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+  $("#get_color").on('click', function(e){
+    e.preventDefault();
+    console.log('this worked');
+    $.ajax({
+      type: 'get',
+      url: '/colors',
+    }).done(function(response) {
+      $("ul li:nth-child(" + response.cell + ")").css('background-color', response.color);
+    });
+  });
 });
